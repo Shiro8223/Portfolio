8365e108d054d815a180c10f221ba059a2c3b5b5
Shiro <16staylor21@gmail.com>
2025-12-17 16:13:56 +0000

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
 
       <div>
         <h1 className="text-pink-600 text-5xl md:text-7xl font-bolder">Shaun Taylor</h1>
-        <p className="mt-4 text-lg md:text-xl">Aspiring Software Engineer</p>
+        <p className="mt-4 text-lg md:text-xl">Software Engineer still in university</p>
         <p className="mt-9 md:text-3xl text-gray-900">Building and enhancing modern applications with passion and precision.</p>
       </div>
 
